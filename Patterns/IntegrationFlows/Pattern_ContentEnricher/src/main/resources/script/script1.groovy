1ba4d80a49b76236936668d9b0ea2b84eecbe1a4
Stefan Boller <stefan.boller@sap.com>
2020-01-21 15:51:55 +0100

    Upload files
    Content for Patterns guidelines
    Empty Folders for other guidelines

---
+++ Patterns/IntegrationFlows/Pattern_ContentEnricher/src/main/resources/script/script1.groovy
@@ -0,0 +1,28 @@
+/*
+ The integration developer needs to create the method processData 
+ This method takes Message object of package com.sap.gateway.ip.core.customdev.util 
+which includes helper methods useful for the content developer:
+The methods available are:
+    public java.lang.Object getBody()
+	public void setBody(java.lang.Object exchangeBody)
+    public java.util.Map<java.lang.String,java.lang.Object> getHeaders()
+    public void setHeaders(java.util.Map<java.lang.String,java.lang.Object> exchangeHeaders)
+    public void setHeader(java.lang.String name, java.lang.Object value)
+    public java.util.Map<java.lang.String,java.lang.Object> getProperties()
+    public void setProperties(java.util.Map<java.lang.String,java.lang.Object> exchangeProperties) 
+       public void setProperty(java.lang.String name, java.lang.Object value)
+ */
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+
+def Message processData(Message message) {
+	map = message.getProperties();
+	def label = map.get("label");
+	if (label==null){
+        label = "payload"
+    }
+	def body = message.getBody(java.lang.String) as String;
+	def messageLog = messageLogFactory.getMessageLog(message);
+	messageLog.addAttachmentAsString(label, body, "text/plain");
+	return message;
+}
