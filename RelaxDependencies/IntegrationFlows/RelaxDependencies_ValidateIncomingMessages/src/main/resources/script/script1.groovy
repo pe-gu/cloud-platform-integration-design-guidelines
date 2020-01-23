bffd485abb9e2eca46b0e9e33f16a615bfe927b3
Gutsche <peter.gutsche@sap.com>
2020-01-23 14:21:15 +0100

    Uploaded Integration Flows and PostmanCollections...
    for RelaxDependencies, PreserveReadability and ManageResources (integrationflows in PreserveReadability still missing)

---
+++ RelaxDependencies/IntegrationFlows/RelaxDependencies_ValidateIncomingMessages/src/main/resources/script/script1.groovy
@@ -0,0 +1,31 @@
+/*
+ The integration developer needs to create the method processData 
+ This method takes Message object of package com.sap.gateway.ip.core.customdev.util 
+which includes helper methods useful for the content developer:
+The methods available are:
+    public java.lang.Object getBody()
+	public void setBody(java.lang.Object exchangeBody)
+    public java.util.Map<java.lang.String,java.lang.Object> getHeaders()
+    public void setHeaders(java.util.Map<java.lang.String,java.lang.Object> exchangeHeaders)
+    public void setHeader(java.lang.String name, java.lang.Object value)
+    public java.util.Map<java.lang.String,java.lang.Object> getProperties()
+    public void setProperties(java.util.Map<java.lang.String,java.lang.Object> exchangeProperties) 
+    public void setProperty(java.lang.String name, java.lang.Object value)
+    public java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> getSoapHeaders()
+    public void setSoapHeaders(java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> soapHeaders) 
+       public void clearSoapHeaders()
+ */
+import com.sap.gateway.ip.core.customdev.util.Message
+import java.nio.charset.StandardCharsets
+
+def Message processData(Message message) {
+       //Headers 
+       def map = message.getHeaders()
+       def rating = map.get("rating")
+       // encode header, which shall be used in OData query as parameter
+       if (rating!=null) {
+           rating = URLEncoder.encode(rating,StandardCharsets.UTF_8.name())
+           message.setHeader("productId", productId)
+       }
+       return message
+}
